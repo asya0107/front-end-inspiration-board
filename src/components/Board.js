0757b532a8281bf8116ce91b763a59e722187e3d
Asya Smith <asyasmith15@gmail.com>
2021-12-23 14:03:44 -0500

    board, boardlist, and newboard form finished

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,13 @@
 const Board = (props) => {
-  const displayBoardInfo = () => {
-    const updatedBoard = {
-      id: props.id,
-      title: props.title,
-      owner: props.owner,
-    };
-    // Invoke the function passed in through the prop named “onUpdate”
-    // This function is referenced by the name “updateStudentData” in App
-    props.onUpdate(updatedBoard);
-  };
-  const showCards = props.isPresent ? "green" : "red";
+    // this is finalized
   return (
-    <div>
-      <ul>
-        <li>Board Title: {props.title}</li>
-      </ul>
-      <button onClick={showCards}>Toggle if {props.name} is present</button>
-    </div>
-  );
-};
+        <div onClick={() => props.onBoardSelect(props.board)}>
+          {props.board.title}
+        </div>
+      );
+    };
+  
+
 Board.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import PropTypes from "prop-types";
 
-const NewBoardForm = (props) => {
+const newBoardForm = (props) => {
    const [formFields, setFormFields] = useState({
        title: '',
        owner: ''
